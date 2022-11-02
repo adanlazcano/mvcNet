13b8d918c23887cbb3180dcd6c910dc2ec7cadcd
Adán Arturo Sánchez Lazcano <adan.sanchez@astrum.com.mx>
2022-11-02 16:04:56 -0600

    first commit

---
+++ mvcCrud/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvcCrud.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Code { get; set; }
+    }
+}
